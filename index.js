0e2e9f174ced68ca565b3c99e5b41c96d268164e
kdylsky <kdy9kdy@gmail.com>
2023-01-18 15:30:34 +0900

    [ADD] 캠핑장 업데이트

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp-project')
@@ -17,7 +18,8 @@ app.set("views",path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //req.body는 비어있는 것이 기본이기 때문에 파싱을 해주어야 한다.
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res)=>{
     res.render("home") 
@@ -45,6 +47,18 @@ app.get("/campgrounds/:id", async(req,res)=>{
     res.render("campgrounds/detail", {campground});
 })
 
+app.get("/campgrounds/:id/edit", async(req,res)=>{
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", {campground});
+})
+
+app.put("/campgrounds/:id", async(req,res)=>{
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new:true});
+    // await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, ()=>{
     console.log("SERVING 3000 PORT")  
