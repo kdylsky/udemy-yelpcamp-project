d1efb32076f80e81e8e1653fbd4d7afdab243b8a
kdylsky <kdy9kdy@gmail.com>
2023-01-26 17:47:54 +0900

    [ADD]image스키마 생성 및 virtual 만들기

--- models/campground.js
+++ models/campground.js
@@ -2,17 +2,24 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const User = require("./user");
 
+// 스키마를 중첩해서 image를 만들자.
+const ImageSchema = new mongoose.Schema({
+    url:String,
+    filename:String
+})
+
+//각각의 이미지에 대해서 속성을 사용할 수 있다.
+//만약 따로 설정하지 않았다면 각각의 이미지에 대해서는 사용할 수 없었다.
+ImageSchema.virtual("thumbnail").get(function(){
+    return this.url.replace("/upload", "/upload/w_200");
+})
+
 const CampgroundSchema = new mongoose.Schema({
     title:{
         type:String,
         required:true
     },
-    images:[
-      {
-        url:String,
-        filename:String
-      }  
-    ],
+    images:[ImageSchema],
     price:{
         type:Number,
         required:true
